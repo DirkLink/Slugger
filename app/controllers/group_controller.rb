7ebe5c337307534a809fa832a28b4809a3bfa943
Dirk Link <dlink87@gmail.com>
2015-07-28 09:30:06 -0400

    fixed group rider if logic

--- app/controllers/group_controller.rb
+++ app/controllers/group_controller.rb
@@ -7,19 +7,19 @@ class GroupController < ApplicationController
     if current_user.group      
       group = current_user.group
       rider = User.find params[:rider_id]
-      if group.rider_one_id
+      if group.rider_one_id == nil
         group.update(rider_one_id: rider.id)
         render json: {group: group} and return
       end
-      if group.rider_two_id
+      if group.rider_two_id == nil
         group.update(rider_two_id: rider.id)
         render json: {group: group} and return
       end
-      if group.rider_three_id
+      if group.rider_three_id == nil
         group.update(rider_three_id: rider.id)
         render json: {group: group} and return
       end
-      if group.rider_four_id
+      if group.rider_four_id == nil
         group.update(rider_four_id: rider.id)
         render json: {group: group} and return
       end 
