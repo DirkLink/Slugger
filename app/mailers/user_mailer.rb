6b3d3abc681c3193de9aaab8922ee405c5f3653d
Dirk Link <dlink87@gmail.com>
2015-08-04 10:23:35 -0400

    fixed variable name error

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,7 +4,7 @@ class UserMailer < ApplicationMailer
   def invite_email user, sender
     @sender = sender
     @recipient = user
-    @confirm_url = "https://sluggr-api.herokuapp.com/group/confirm?inviter=#{@sender.id}&invitee=#{@user.id}&Email=#{@sender.email}"
+    @confirm_url = "https://sluggr-api.herokuapp.com/group/confirm?inviter=#{@sender.id}&invitee=#{@recipient.id}&Email=#{@sender.email}"
     @deny_url = "https://sluggr-api.herokuapp.com/group/deny?Email=#{@sender.email}"
     mail(to: @recipient.email, subject: "#{@sender.email} has invited you to join his group.")
   end
